61bc7cae785f5a26a6b197c31926d477cb9428f0
Liam DeWitt <mugzy550@gmail.com>
2023-03-03 20:41:54 -0500

    add if statement within while loop to check for list. add console to print index number of each item and the strings associated

--- app.js
+++ app.js
@@ -3,8 +3,17 @@
 // "delete" - remove specific to do
 // "quit" - quits app
 let input = prompt("What would you like to do?")
-const toDo = ['']
+const toDo = ['stuff', 'things']
 while (input.toLowerCase() !== "quit" && input.toLowerCase() !== "q"){
+    if (input.toLowerCase() === 'list'){
+        console.log("<---------------->")
+        for (i=0; i < toDo.length; i++){
+            console.log(`${i}: ${toDo[i]}`)
+            // reminder that i logs the index
+        }
+        console.log("<---------------->")
+    }
+
     input = prompt("What would you like to do?")
 }
 console.log("Okay, you quit the app!")
