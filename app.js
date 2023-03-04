54743ea14bdc868a233ca6efc8098c15d6aec57d
Liam DeWitt <mugzy550@gmail.com>
2023-03-03 19:32:25 -0500

    make boiler plate for html and add commands add prompt to js add prompt to user add while loop and quit command

---
+++ app.js
@@ -0,0 +1,10 @@
+// "new" - adds a to do
+// "list" - lists all todos
+// "delete" - remove specific to do
+// "quit" - quits app
+let input = prompt("What would you like to do?")
+const toDo = ['']
+while (input.toLowerCase() !== "quit"){
+    input = prompt("What would you like to do?")
+}
+console.log("Okay, you quit the app!")
