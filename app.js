229c5ee2376353b8d7fe4ba95f15fe4cdae4a5ea
Liam DeWitt <mugzy550@gmail.com>
2023-03-03 21:27:44 -0500

    add empty const array 'toDo' add else if to add a new todo to array add delete option by splicing

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 // "list" - lists all todos
 // "delete" - remove specific to do
 // "quit" - quits app
-let input = prompt("What would you like to do?")
-const toDo = ['stuff', 'things']
+let input = prompt("What would you like to do?");
+// using let because input will be re-assigned
+const toDo = []
 while (input.toLowerCase() !== "quit" && input.toLowerCase() !== "q"){
     if (input.toLowerCase() === 'list'){
-        console.log("<---------------->")
+        // console.log("--------------------------------");
         for (i=0; i < toDo.length; i++){
-            console.log(`${i}: ${toDo[i]}`)
+            console.log(`${i}: ${toDo[i]}`);
             // reminder that i logs the index
         }
-        console.log("<---------------->")
+        console.log("--------------------------------");
+    } else if (input.toLowerCase() === 'new'){
+        const newTodo = prompt("Okay, what is the new to do?");
+        toDo.push(newTodo);
+    } else if (input.toLowerCase() === 'delete'){
+        const deleteToDo = prompt("Okay, type the index to which you want to delete");
+        toDo.splice(deleteToDo,1)
     }
 
     input = prompt("What would you like to do?")
+    //input being reassigned here too
 }
-console.log("Okay, you quit the app!")
+console.log("Okay, you quit the app!");
