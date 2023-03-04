49dd2d5f4858aca14c32b89b11df3a0208b1676f
Liam DeWitt <mugzy550@gmail.com>
2023-03-03 22:32:07 -0500

    add console log for new to do list input add console log for name of to do to print add if statement for when index number is NaN, it returns an error

--- app.js
+++ app.js
@@ -7,21 +7,28 @@ let input = prompt("What would you like to do?");
 const toDo = []
 while (input.toLowerCase() !== "quit" && input.toLowerCase() !== "q"){
     if (input.toLowerCase() === 'list'){
-        // console.log("--------------------------------");
+        console.log("--------------------------------");
         for (i=0; i < toDo.length; i++){
             console.log(`${i}: ${toDo[i]}`);
             // reminder that i logs the index
         }
         console.log("--------------------------------");
     } else if (input.toLowerCase() === 'new'){
-        const newTodo = prompt("Okay, what is the new to do?");
-        toDo.push(newTodo);
+        const newToDo = prompt("Okay, what is the new to do?");
+        toDo.push(newToDo);
+        console.log(`${newToDo} added to list`)
     } else if (input.toLowerCase() === 'delete'){
-        const deleteToDo = prompt("Okay, type the index to which you want to delete");
-        toDo.splice(deleteToDo,1)
+        const deleteToDo = parseInt(prompt("Okay, type the index to which you want to delete"));
+        //putting parse int ^ because if it returns 'NaN' it defaults to index 0
+        if (!Number.isNaN(deleteToDo)){
+        const deleted = toDo.splice(deleteToDo,1);
+        console.log(`${deleted} deleted...`);
+        } else {
+            console.log("Unknown index");
+        }
     }
 
-    input = prompt("What would you like to do?")
+    input = prompt("What would you like to do?");
     //input being reassigned here too
 }
 console.log("Okay, you quit the app!");
